41e6d944366f655bf2ff850f2bf1974731eca6a0
Nirja Basawa <nbasawa@ucsc.edu>
2026-05-10 19:02:44 -0700

    text field must be nonempty, login navigate to home

--- lib/screens/login/signup_screen.dart
+++ lib/screens/login/signup_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../theme/app_colors.dart';
-import '../widgets/primary_button.dart';
+import '../../theme/app_colors.dart';
+import '../../widgets/primary_button.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
